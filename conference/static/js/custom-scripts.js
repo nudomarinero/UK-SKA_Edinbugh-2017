dfe73dd5e96ec96e672d75e751cd42655e4dd4fe
José Sabater Montes <jsm@iaa.es>
2017-07-16 20:07:41 +0100

    Fix template

--- conference/static/js/custom-scripts.js
+++ conference/static/js/custom-scripts.js
@@ -68,10 +68,10 @@
          * Top Navigaion Init
          * =======================================
          */
-        var navigation = $('#js-navbar-menu').okayNav({
-            toggle_icon_class: "okayNav__menu-toggle",
-            toggle_icon_content: "<span /><span /><span /><span /><span />"
-        });
+        // var navigation = $('#js-navbar-menu').okayNav({
+        //     toggle_icon_class: "okayNav__menu-toggle",
+        //     toggle_icon_content: "<span /><span /><span /><span /><span />"
+        // });
 
 
 
@@ -100,28 +100,43 @@
          * NAVIGATION SCROLL
          * =======================================
          */
-        var TopOffsetId = '.navbar-brand';
-        $('#js-navbar-menu').onePageNav({
-            currentClass: 'active',
-            scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
-            scrollSpeed: 1000,
-            scrollOffset: Math.abs($(TopOffsetId).outerHeight() - 1)
+
+        // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
         });
-
-        $('.btn-scroll a, a.btn-scroll').on('click', function(e) {
-            e.preventDefault();
-
-            var target = this.hash,
-                scrollOffset = Math.abs($(TopOffsetId).outerHeight()),
-                $target = ($(target).offset() || { "top": NaN }).top;
-
-            $('html, body').stop().animate({
-                'scrollTop': $target - scrollOffset
-            }, 900, 'swing', function() {
-                window.location.hash = target;
-            });
-
-        });
+      }
+    }
+  });
 
 
     });
