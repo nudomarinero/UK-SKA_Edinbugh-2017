a4e8d7ff2bb0812e3a3180e97cecf6dda32970be
José Sabater Montes <jsm@iaa.es>
2017-08-17 17:10:27 +0100

    Enhance the admin interface

--- conference/admin.py
+++ conference/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Participant
 
-admin.site.register(Participant)
 
+class ParticipantAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Personal information", {'fields': ['email', 'name', 'institution', 'assistant']}),
+        ("Asistance", {'fields': ['assistant', 'participant_hash']}),
+        ("Contribution", {'fields': ['contribution', 'title', 'abstract', 'link']}),
+        ]
+    list_display = ('email', 'name', 'assistant', 'contribution', 'title')
+    list_filter = ['assistant', 'contribution']
+
+admin.site.register(Participant, ParticipantAdmin)
